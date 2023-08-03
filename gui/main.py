92b1464868a58637276950e74e8533afa7626641
Nikita Vorkov <vorkov711@gmail.com>
2023-08-04 01:11:16 +0300

    first norm gui

--- gui/main.py
+++ gui/main.py
@@ -3,8 +3,9 @@ from threading import Thread
 import time
 import json
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QLabel, QGridLayout, QLineEdit, QCheckBox
+from PyQt5.QtGui import QPixmap
 
 statistics = {
     "Anger": 0,
@@ -24,52 +25,84 @@ class EmulGui(QWidget):
     def __init__(self):
         super().__init__()
 
-
-        img = QPixmap("./files/img.png")
-        pixLbl = QLabel()
-        pixLbl.setPixmap(img)
-        lbl1 = QLabel('Ворков\nНикита')
-
-        lbl111 = QLabel('Anger: ')
-        lbl111 = QLabel('unknown')
-        lbl121 = QLabel('Fear: ')
-        lbl121 = QLabel('unknown')
-        lbl131 = QLabel('Happy: ')
-        lbl131 = QLabel('unknown')
-        lbl141 = QLabel('Neutral: ')
-        lbl141 = QLabel('unknown')
-        lbl151 = QLabel('Sadness: ')
-        lbl151 = QLabel('unknown')
-        lbl161 = QLabel('Surprise: ')
-        lbl161 = QLabel('unknown')
-        label->setText(text);
-
-        img1 = QPixmap("./files/img_1.png")
+        img1 = QPixmap("files/img1.png")
+        img1 = img1.scaled(QSize(250, 250))
         pixLbl1 = QLabel()
         pixLbl1.setPixmap(img1)
-        lbl2 = QLabel('Иван\nКостылев')
+        lbl1 = QLabel('Ворков Никита')
 
+        lbl111 = QLabel('Anger: ')
+        lbl112 = QLabel('20 %')
+        lbl121 = QLabel('Fear: ')
+        lbl122 = QLabel('30 %')
+        lbl131 = QLabel('Happy: ')
+        lbl132 = QLabel('25 %')
+        lbl141 = QLabel('Neutral: ')
+        lbl142 = QLabel('10 %')
+        lbl151 = QLabel('Sadness: ')
+        lbl152 = QLabel('40 %')
+        lbl161 = QLabel('Surprise: ')
+        lbl162 = QLabel('12 %')
+        # label->setText(text);
 
-#############################################
+        img2 = QPixmap("./files/img2.png")
+        img2 = img2.scaled(QSize(250, 250))
+        pixLbl2 = QLabel()
+        pixLbl2.setPixmap(img2)
+        lbl2 = QLabel('Иван Костылев')
 
-        grid.addWidget(pixLbl, 1, 0, 4, 4)
-        grid.addWidget(lbl1, 3, 0, 2, 4)
+        lbl211 = QLabel('Anger: ')
+        lbl212 = QLabel('12 %')
+        lbl221 = QLabel('Fear: ')
+        lbl222 = QLabel('17 %')
+        lbl231 = QLabel('Happy: ')
+        lbl232 = QLabel('22 %')
+        lbl241 = QLabel('Neutral: ')
+        lbl242 = QLabel('34 %')
+        lbl251 = QLabel('Sadness: ')
+        lbl252 = QLabel('45 %')
+        lbl261 = QLabel('Surprise: ')
+        lbl262 = QLabel('23 %')
 
-        grid.addWidget(lbl111, 1, 3, 3, 1)
-        grid.addWidget(lbl111, 1, 3, 3, 1)
-        grid.addWidget(lbl121, 1, 3, 3, 1)
-        grid.addWidget(lbl121, 1, 3, 3, 1)
-        grid.addWidget(lbl131, 1, 3, 3, 1)
-        grid.addWidget(lbl131, 1, 3, 3, 1)
-        grid.addWidget(lbl141, 1, 3, 3, 1)
-        grid.addWidget(lbl141, 1, 3, 3, 1)
-        grid.addWidget(lbl151, 1, 3, 3, 1)
-        grid.addWidget(lbl151, 1, 3, 3, 1)
-        grid.addWidget(lbl161, 1, 3, 3, 1)
-        grid.addWidget(lbl161, 1, 3, 3, 1)
+        empty = QLabel('')
 
-        grid.addWidget(pixLbl1, 4, 0, 4, 4)
-        grid.addWidget(lbl2, 6, 0, 2, 4)
+        grid = QGridLayout()
+        grid.setSpacing(5)
+
+        grid.addWidget(pixLbl1, 1, 0, 6, 6)
+        grid.addWidget(lbl1, 7, 0, 1, 4)
+
+        grid.addWidget(lbl111, 1, 5, 1, 1)
+        grid.addWidget(lbl112, 1, 6, 1, 1)
+        grid.addWidget(lbl121, 2, 5, 1, 1)
+        grid.addWidget(lbl122, 2, 6, 1, 1)
+        grid.addWidget(lbl131, 3, 5, 1, 1)
+        grid.addWidget(lbl132, 3, 6, 1, 1)
+        grid.addWidget(lbl141, 4, 5, 1, 1)
+        grid.addWidget(lbl142, 4, 6, 1, 1)
+        grid.addWidget(lbl151, 5, 5, 1, 1)
+        grid.addWidget(lbl152, 5, 6, 1, 1)
+        grid.addWidget(lbl161, 6, 5, 1, 1)
+        grid.addWidget(lbl162, 6, 6, 1, 1)
+
+        grid.addWidget(empty, 8, 0)
+        grid.addWidget(empty, 8, 0)
+
+        grid.addWidget(pixLbl2, 9, 0, 6, 6)
+        grid.addWidget(lbl2, 15, 0, 1, 4)
+        
+        grid.addWidget(lbl211, 9, 5, 1, 1)
+        grid.addWidget(lbl212, 9, 6, 1, 1)
+        grid.addWidget(lbl221, 10, 5, 1, 1)
+        grid.addWidget(lbl222, 10, 6, 1, 1)
+        grid.addWidget(lbl231, 11, 5, 1, 1)
+        grid.addWidget(lbl232, 11, 6, 1, 1)
+        grid.addWidget(lbl241, 12, 5, 1, 1)
+        grid.addWidget(lbl242, 12, 6, 1, 1)
+        grid.addWidget(lbl251, 13, 5, 1, 1)
+        grid.addWidget(lbl252, 13, 6, 1, 1)
+        grid.addWidget(lbl261, 14, 5, 1, 1)
+        grid.addWidget(lbl262, 14, 6, 1, 1)
 
         self.setLayout(grid)
         self.setGeometry(300, 300, 720, 480)
